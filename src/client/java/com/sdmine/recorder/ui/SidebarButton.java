280fd38befe2cc4587ade9cb6d4fc13e687e5698
twiceextra57863-source <twiceextra57863@gmail.com>
2026-02-15 23:32:13 +0530

    Update SidebarButton.java

--- src/client/java/com/sdmine/recorder/ui/SidebarButton.java
+++ src/client/java/com/sdmine/recorder/ui/SidebarButton.java
@@ -7,7 +7,7 @@ public class SidebarButton {
 
     public int x, y, width, height;
     public SidebarSection section;
-    public boolean hovered;
+    private boolean hovered;
 
     public SidebarButton(int x, int y, int width, int height, SidebarSection section) {
         this.x = x;
@@ -18,25 +18,28 @@ public class SidebarButton {
     }
 
     public void render(DrawContext ctx, int mouseX, int mouseY, boolean active) {
-        hovered = mouseX >= x && mouseX <= x + width && mouseY >= y && mouseY <= y + height;
 
-        int bgColor;
-        if (active) bgColor = 0xFF3A86FF;
-        else if (hovered) bgColor = 0xFF2F2F2F;
-        else bgColor = 0xFF1E1E1E;
+        hovered = mouseX >= x && mouseX <= x + width
+                && mouseY >= y && mouseY <= y + height;
 
-        ctx.fill(x, y, x + width, y + height, bgColor);
+        int color;
+        if (active) color = 0xFF3A86FF;
+        else if (hovered) color = 0xFF333333;
+        else color = 0xFF1E1E1E;
+
+        ctx.fill(x, y, x + width, y + height, color);
 
         ctx.drawText(
                 ctx.getMatrices().peek().getPositionMatrix(),
                 Text.literal(section.name()),
                 x + 10,
-                y + 8,
+                y + 10,
                 0xFFFFFF
         );
     }
 
     public boolean isClicked(double mouseX, double mouseY) {
-        return mouseX >= x && mouseX <= x + width && mouseY >= y && mouseY <= y + height;
+        return mouseX >= x && mouseX <= x + width
+                && mouseY >= y && mouseY <= y + height;
     }
 }
