7aa73b378e27296daba7143987707873ad5a8087
twiceextra57863-source <twiceextra57863@gmail.com>
2026-02-15 23:39:29 +0530

    Update ProjectData.java

--- src/client/java/com/sdmine/recorder/project/ProjectData.java
+++ src/client/java/com/sdmine/recorder/project/ProjectData.java
@@ -5,12 +5,13 @@ public class ProjectData {
     public String name;
     public long createdAt;
     public long lastOpened;
-    public String mode;
+    public int fps;
+    public String quality;
+    public String template;
 
     public ProjectData(String name) {
         this.name = name;
         this.createdAt = System.currentTimeMillis();
-        this.lastOpened = this.createdAt;
-        this.mode = "recording";
+        this.lastOpened = createdAt;
     }
 }
