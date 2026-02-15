45aa8846ad270b18a14388034d1a97ed732297c8
twiceextra57863-source <twiceextra57863@gmail.com>
2026-02-15 23:29:04 +0530

    Create ProjectData.java

---
+++ src/client/java/com/sdmine/recorder/project/ProjectData.java
@@ -0,0 +1,16 @@
+package com.sdmine.recorder.project;
+
+public class ProjectData {
+    public String name;
+    public long createdAt;
+    public long lastOpened;
+    public String mode;
+
+    public ProjectData(String name) {
+        this.name = name;
+        this.createdAt = System.currentTimeMillis();
+        this.lastOpened = createdAt;
+        this.mode = "recording";
+    }
+}
+
