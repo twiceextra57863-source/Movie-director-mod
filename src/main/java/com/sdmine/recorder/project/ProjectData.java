1f0d56868d2735b855a0308774227928bd64184f
twiceextra57863-source <twiceextra57863@gmail.com>
2026-02-16 00:57:48 +0530

    Update ProjectData.java

--- src/main/java/com/sdmine/recorder/project/ProjectData.java
+++ src/main/java/com/sdmine/recorder/project/ProjectData.java
@@ -1,17 +1,20 @@
 package com.sdmine.recorder.project;
 
+import java.util.UUID;
+
 public class ProjectData {
 
+    public final String id;
     public String name;
-    public long createdAt;
-    public long lastOpened;
     public int fps;
     public String quality;
-    public String template;
+    public long createdAt;
 
-    public ProjectData(String name) {
+    public ProjectData(String name, int fps, String quality) {
+        this.id = UUID.randomUUID().toString();
         this.name = name;
+        this.fps = fps;
+        this.quality = quality;
         this.createdAt = System.currentTimeMillis();
-        this.lastOpened = createdAt;
     }
 }
