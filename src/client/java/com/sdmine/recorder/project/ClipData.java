e74a3277b5b3b1a1b25cc3f991300c3c1099cc4b
twiceextra57863-source <twiceextra57863@gmail.com>
2026-02-16 06:26:00 +0530

    Update ClipData.java

--- src/client/java/com/sdmine/recorder/project/ClipData.java
+++ src/client/java/com/sdmine/recorder/project/ClipData.java
@@ -2,27 +2,27 @@ package com.sdmine.recorder.project;
 
 public class ClipData {
 
-    public enum ClipType {
+    public enum Type {
         IDLE_CAMERA,
         KEYFRAME_CAMERA
     }
 
-    private ClipType type;
+    private Type type;
     private int startTick;
     private int duration;
 
-    public ClipData(ClipType type, int startTick, int duration) {
+    public ClipData(Type type, int startTick, int duration) {
         this.type = type;
         this.startTick = startTick;
         this.duration = duration;
     }
 
-    public ClipType getType() {
+    public Type getType() {
         return type;
     }
 
     public void convertToKeyframe() {
-        this.type = ClipType.KEYFRAME_CAMERA;
+        this.type = Type.KEYFRAME_CAMERA;
     }
 
     public int getStartTick() {
@@ -33,4 +33,3 @@ public class ClipData {
         return duration;
     }
 }
-
