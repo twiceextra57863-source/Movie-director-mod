cffc267a41c4a0ed68621bd50f3a7480344f198a
twiceextra57863-source <twiceextra57863@gmail.com>
2026-02-16 06:51:28 +0530

    Update SidebarButton.java

--- src/client/java/com/sdmine/recorder/ui/widgets/SidebarButton.java
+++ src/client/java/com/sdmine/recorder/ui/widgets/SidebarButton.java
@@ -1,35 +1,34 @@
 package com.sdmine.recorder.ui.widgets;
 
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.widget.ClickableWidget;
 import net.minecraft.text.Text;
+import net.minecraft.client.gui.screen.narration.NarrationMessageBuilder;
 
 public class SidebarButton extends ClickableWidget {
 
-    private final SidebarSection section;
+    public final SidebarSection section;
 
-    public SidebarButton(int x, int y, int width, int height, SidebarSection section) {
-        super(x, y, width, height, Text.literal(section.name()));
+    public SidebarButton(int x, int y, int w, int h, SidebarSection section) {
+        super(x, y, w, h, Text.literal(section.name()));
         this.section = section;
     }
 
     @Override
-    protected void renderWidget(DrawContext ctx, int mouseX, int mouseY, float delta) {
-        int color = isHovered() ? 0xFF555555 : 0xFF333333;
+    protected void renderButton(DrawContext ctx, int mouseX, int mouseY, float delta) {
+        int color = isHovered() ? 0xFF444444 : 0xFF222222;
         ctx.fill(getX(), getY(), getX() + width, getY() + height, color);
-
-        ctx.drawText(
-                MinecraftClient.getInstance().textRenderer,
+        ctx.drawTextWithShadow(
+                client.textRenderer,
                 getMessage(),
                 getX() + 6,
                 getY() + 6,
-                0xFFFFFF,
-                false
+                0xFFFFFF
         );
     }
 
     @Override
-    public void onClick(double mouseX, double mouseY) {
+    protected void appendClickableNarrations(NarrationMessageBuilder builder) {
+        appendDefaultNarrations(builder);
     }
 }
