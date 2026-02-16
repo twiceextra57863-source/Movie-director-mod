ee581246be1f12d730e61f06242048bbd721ae07
twiceextra57863-source <twiceextra57863@gmail.com>
2026-02-16 07:08:29 +0530

    Update SidebarButton.java

--- src/client/java/com/sdmine/recorder/ui/widgets/SidebarButton.java
+++ src/client/java/com/sdmine/recorder/ui/widgets/SidebarButton.java
@@ -1,34 +1,37 @@
 package com.sdmine.recorder.ui.widgets;
 
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.widget.ClickableWidget;
 import net.minecraft.text.Text;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.client.gui.screen.narration.NarrationMessageBuilder;
 
 public class SidebarButton extends ClickableWidget {
 
     public final SidebarSection section;
 
-    public SidebarButton(int x, int y, int w, int h, SidebarSection section) {
-        super(x, y, w, h, Text.literal(section.name()));
+    public SidebarButton(int x, int y, int width, int height, SidebarSection section) {
+        super(x, y, width, height, Text.literal(section.name()));
         this.section = section;
     }
 
     @Override
-    protected void renderButton(DrawContext ctx, int mouseX, int mouseY, float delta) {
-        int color = isHovered() ? 0xFF444444 : 0xFF222222;
-        ctx.fill(getX(), getY(), getX() + width, getY() + height, color);
-        ctx.drawTextWithShadow(
-                client.textRenderer,
-                getMessage(),
-                getX() + 6,
-                getY() + 6,
+    protected void renderWidget(DrawContext context, int mouseX, int mouseY, float delta) {
+        int bg = this.isHovered() ? 0xFF333333 : 0xFF222222;
+        context.fill(getX(), getY(), getX() + width, getY() + height, bg);
+
+        context.drawCenteredTextWithShadow(
+                MinecraftClient.getInstance().textRenderer,
+                this.getMessage(),
+                getX() + width / 2,
+                getY() + (height - 8) / 2,
                 0xFFFFFF
         );
     }
 
     @Override
     protected void appendClickableNarrations(NarrationMessageBuilder builder) {
-        appendDefaultNarrations(builder);
+        this.appendDefaultNarrations(builder);
     }
 }
