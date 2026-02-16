48ec942b285856d6bf5d7829564e2ba3fb1e346a
twiceextra57863-source <twiceextra57863@gmail.com>
2026-02-16 06:43:45 +0530

    Update projectData.java

--- src/client/java/com/sdmine/recorder/project/projectData.java
+++ src/client/java/com/sdmine/recorder/project/projectData.java
@@ -1,29 +1,16 @@
 package com.sdmine.recorder.project;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
-
 public class ProjectData {
 
-    private final String id;
     public String name;
     public int fps;
     public String quality;
     public String template;
 
-    private final List<ClipData> clips = new ArrayList<>();
-
     public ProjectData(String name) {
-        this.id = UUID.randomUUID().toString();
         this.name = name;
-    }
-
-    public void addClip(ClipData clip) {
-        clips.add(clip);
-    }
-
-    public List<ClipData> getClips() {
-        return clips;
+        this.fps = 60;
+        this.quality = "High";
+        this.template = "Default";
     }
 }
