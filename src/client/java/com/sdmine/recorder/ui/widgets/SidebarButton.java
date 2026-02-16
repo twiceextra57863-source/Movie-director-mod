2abe5065db6c6e1fe77619c0080efa200186417f
twiceextra57863-source <twiceextra57863@gmail.com>
2026-02-16 06:35:19 +0530

    Update SidebarButton.java

--- src/client/java/com/sdmine/recorder/ui/widgets/SidebarButton.java
+++ src/client/java/com/sdmine/recorder/ui/widgets/SidebarButton.java
@@ -1,45 +1,35 @@
 package com.sdmine.recorder.ui.widgets;
 
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.widget.ClickableWidget;
 import net.minecraft.text.Text;
 
-public class SidebarButton {
+public class SidebarButton extends ClickableWidget {
 
-    public int x, y, width, height;
-    public SidebarSection section;
-    private boolean hovered;
+    private final SidebarSection section;
 
     public SidebarButton(int x, int y, int width, int height, SidebarSection section) {
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
+        super(x, y, width, height, Text.literal(section.name()));
         this.section = section;
     }
 
-    public void render(DrawContext ctx, int mouseX, int mouseY, boolean active) {
-
-        hovered = mouseX >= x && mouseX <= x + width
-                && mouseY >= y && mouseY <= y + height;
-
-        int color;
-        if (active) color = 0xFF3A86FF;
-        else if (hovered) color = 0xFF333333;
-        else color = 0xFF1E1E1E;
-
-        ctx.fill(x, y, x + width, y + height, color);
+    @Override
+    protected void renderWidget(DrawContext ctx, int mouseX, int mouseY, float delta) {
+        int color = isHovered() ? 0xFF555555 : 0xFF333333;
+        ctx.fill(getX(), getY(), getX() + width, getY() + height, color);
 
         ctx.drawText(
-                ctx.getMatrices().peek().getPositionMatrix(),
-                Text.literal(section.name()),
-                x + 10,
-                y + 10,
-                0xFFFFFF
+                MinecraftClient.getInstance().textRenderer,
+                getMessage(),
+                getX() + 6,
+                getY() + 6,
+                0xFFFFFF,
+                false
         );
     }
 
-    public boolean isClicked(double mouseX, double mouseY) {
-        return mouseX >= x && mouseX <= x + width
-                && mouseY >= y && mouseY <= y + height;
+    @Override
+    public void onClick(double mouseX, double mouseY) {
     }
 }
