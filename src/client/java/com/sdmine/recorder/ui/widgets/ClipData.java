ef140698513822f698fc89352636f660a4d2266c
twiceextra57863-source <twiceextra57863@gmail.com>
2026-02-16 07:17:18 +0530

    Update ClipData.java

--- src/client/java/com/sdmine/recorder/ui/widgets/ClipData.java
+++ src/client/java/com/sdmine/recorder/ui/widgets/ClipData.java
@@ -2,57 +2,25 @@ package com.sdmine.recorder.ui.widgets;
 
 public class ClipData {
 
-    /* =========================
-       CLIP TYPE (IMPORTANT)
-       ========================= */
-    public enum Type {
-        IDLE_CAMERA,
-        CAMERA_MOVE,
-        CUT,
-        EFFECT
-    }
+    private final String name;
+    private final int start;
+    private final int length;
 
-    /* =========================
-       FIELDS
-       ========================= */
-    public final String name;
-    public final Type type;
-    public final int start;
-    public final int end;
-
-    private boolean keyframe;
-
-    /* =========================
-       CONSTRUCTORS
-       ========================= */
-
-    // Simple constructor (old usage support)
-    public ClipData(String name, int start, int end) {
-        this(name, Type.IDLE_CAMERA, start, end);
-    }
-
-    // Full constructor (new system)
-    public ClipData(String name, Type type, int start, int end) {
+    public ClipData(String name, int start, int length) {
         this.name = name;
-        this.type = type;
         this.start = start;
-        this.end = end;
-        this.keyframe = false;
+        this.length = length;
     }
 
-    /* =========================
-       METHODS
-       ========================= */
-
-    public int length() {
-        return end - start;
+    public String getName() {
+        return name;
     }
 
-    public boolean isKeyframe() {
-        return keyframe;
+    public int getStart() {
+        return start;
     }
 
-    public void convertToKeyframe() {
-        this.keyframe = true;
+    public int getLength() {
+        return length;
     }
 }
